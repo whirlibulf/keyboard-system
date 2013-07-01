10609a20ddcb1b10a0888ecb2c1f04897c141436
whirlibulf <whirlibulf.engine@gmail.com>
2013-07-01 23:03:43 +0100

    Use engine config instead of passing options parameter

--- index.js
+++ index.js
@@ -1,44 +1,49 @@
 var keycode = require("keycode");
 
-function System(options) {
-  var that = this;
-  this.keys = [];
-
-  options = options || {};
-  options.stop = options.stop || [];
-
-  document.addEventListener("keydown", function (e) {
-    var key = keycode(e.keyCode || e.which);
-
-    if (options.stop.indexOf(key) !== -1) {
-      e.preventDefault();
-      e.stopPropagation();
-    }
-
-    that.keys[key] = true;
-  });
-
-  document.addEventListener("keyup", function (e) {
-    var key = keycode(e.keyCode || e.which);
-
-    if (options.stop.indexOf(key) !== -1) {
-      e.preventDefault();
-      e.stopPropagation();
-    }
-
-    that.keys[key] = false;
-  });
+function System(config) {
+    this.config = config || "keyboard";
+    this.keys = [];
 }
 
 System.prototype.init = function (engine) {
-  var that = this;
+    var that,
+        options;
 
-  console.log("Keyboard system loaded");
-  this.engine = engine;
+    that = this;
 
-  this.engine.use('keyboard', function (options) {
-    return that.keys;
-  });
+    console.log("Keyboard system loaded");
+    this.engine = engine;
+
+    options = this.engine.config(this.config);
+    if (!options.stop) {
+        options.stop = [];
+    }
+
+    document.addEventListener("keydown", function (e) {
+        var key = keycode(e.keyCode || e.which);
+
+        if (options.stop.indexOf(key) !== -1) {
+            e.preventDefault();
+            e.stopPropagation();
+        }
+
+        that.keys[key] = true;
+    });
+
+    document.addEventListener("keyup", function (e) {
+        var key = keycode(e.keyCode || e.which);
+
+        if (options.stop.indexOf(key) !== -1) {
+            e.preventDefault();
+            e.stopPropagation();
+        }
+
+        that.keys[key] = false;
+    });
+
+    this.engine.use('keyboard', function (options) {
+        return that.keys;
+    });
 };
 
 module.exports = System;
